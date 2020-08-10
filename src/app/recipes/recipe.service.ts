80952b79c5cec37415ddc46e3ae36530984073e6
Alex Johnson <ajohnson2389@gmail.com>
2020-08-09 21:46:33 -0400

    Update recipe.service.ts

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -3,7 +3,6 @@ import { Recipe } from './recipe.model';
 
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
-    
     private recipes: Recipe[] = [
         new Recipe('A Test Recipe', 'This is simply a test', 'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2018/9/26/0/FNK_Tuscan-Chicken-Skillet_H2_s4x3.jpg.rend.hgtvcom.826.620.suffix/1537973085542.jpeg'),
         new Recipe('Another Test Recipe', 'This is simply a test', 'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2018/9/26/0/FNK_Tuscan-Chicken-Skillet_H2_s4x3.jpg.rend.hgtvcom.826.620.suffix/1537973085542.jpeg')
