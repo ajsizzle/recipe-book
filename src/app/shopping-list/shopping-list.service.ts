e60813e10e953a736f8b71f1a1bbd53cbdd7d865
Alex Johnson <ajohnson2389@gmail.com>
2020-08-15 13:08:22 -0400

    Load Shopping-List items to the form

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -11,6 +11,11 @@ export class ShoppingListService {
     getIngredients() {
         return this.ingredients.slice();
     }
+
+    getIngredient(index: number) {
+        return this.ingredients[index];
+    }
+
     addIngredient(ingredient: Ingredient) {
         this.ingredients.push(ingredient);
         this.ingredientsChanged.next(this.ingredients.slice());
